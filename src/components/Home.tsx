79c89ac1a2f8ce0d4eb4f39f43b0a2380225c07c
Hunter Kang <hunterkang5@g.ucla.edu>
2024-05-21 20:56:43 -0700

    19_landingPage_leftCol

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,20 @@
+import '../styles/Home.scss';
+import Button from '@mui/material/Button';
+
 function Home(): JSX.Element {
-    return <div>Content of Home</div>;
+    return ( 
+        <div id="col-1">
+            <h1 id="title">Cookie Jar</h1>
+            <p id="description">
+                What are web cookies? 
+                What types of cookies are out there? 
+                Are cookies safe?
+                Let's embark on a delicious adventure to learn all about web cookies. Bon Appétit!            
+            </p>
+            <Button sx={{backgroundColor:'white', color: 'black', '&:hover': {backgroundColor: 'white'}}} size="large" variant="contained">Menu</Button>
+        </div>
+ );
 }
+
 
 export default Home;
