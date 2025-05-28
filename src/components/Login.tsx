ae10644ca894bc96a83cd4d00b455d40526c3488
charlenehuang1 <155873161+charlenehuang1@users.noreply.github.com>
2025-05-27 19:30:01 -0700

    Login and SignUp pages and CSS files (#44)

---
+++ src/components/Login.tsx
@@ -0,0 +1,21 @@
+import React,  { useState, useEffect } from 'react';
+import '../styles/Home.scss';
+import '../styles/App.scss';
+import '../styles/Login.scss'
+import Button from '@mui/material/Button';
+
+
+const Login: React.FC = () => {
+  return (
+    <div className='login'>
+      <h1> Login to Start Baking! </h1>
+      <p> Email </p>
+      <input type="email"></input>
+      <p> Password </p>
+      <input type="password"></input>
+      <Button className='login-button'>Login</Button>
+    </div>
+  );
+};
+
+export default Login;
