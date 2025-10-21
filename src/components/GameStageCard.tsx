83f58405ea9d0284c8623e5b6ae455452f70e1df
Derek H <derekh4000@gmail.com>
2025-10-21 03:44:28 -0700

    Add styling to the Game Stage Card component in the Game Stages page, and improve spacing of the page

--- src/components/GameStageCard.tsx
+++ src/components/GameStageCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './../styles/Cookie.scss';
+import "../styles/GameStages.scss";
 import { Link } from 'react-router-dom';
 
 interface GameStageCardProps {
@@ -10,10 +11,12 @@ interface GameStageCardProps {
 
 const GameStageCard: React.FC<GameStageCardProps> = ({ to, name, description }) => {
   return (
-    <div className="gamestagecard">
-        <Link to={to}>
-            <div>{name}</div>
-            <div>{description}</div>
+    <div style={{ margin: "16px" }}>
+        <Link to={to} style={{ textDecoration: "none" }}>
+            <div className="game-stage-card">
+                <div className="game-stage-card-title">{name}</div>
+                <div className="game-stage-card-desc">{description}</div>
+            </div>
         </Link>
     </div>
   );
--- src/components/GameStages.tsx
+++ src/components/GameStages.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import GameStageCard from "./GameStageCard";
+import "../styles/GameStages.scss";
 
 const GameStages: React.FC = () => {
   return (
-    <div>
+    <div className="game-stage-cards-container">
         <GameStageCard
             to="/stage1"
             name="Stage 1"
-            description="Doing stuff in Stage 1."
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <GameStageCard
             to="/stage2"
             name="Stage 2"
-            description="Doing stuff in Stage 2."
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
     </div>
   );
