484673ab0cb79a063c8836a9ed87e69063b9df94
GeeseAndGoosey <williamdiao03@gmail.com>
2025-10-21 19:44:24 -0700

    Create Stage3.tsx

---
+++ src/components/stages/Stage3.tsx
@@ -0,0 +1,16 @@
+//Falling cookie game
+import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+
+
+const Stage3: React.FC = () =>{
+
+    return(
+        <div className = "stage-3-container">
+            <h1>First Party vs Third Party Cookies</h1>
+            
+        </div>
+
+    )
+}
