54acfcd7ff7fcbd13ab9c76e283a8ce7cd0a4611
GeeseAndGoosey <williamdiao03@gmail.com>
2025-10-25 02:55:43 -0700

    Added basket movement mechanics

--- src/components/stages/Stage3.tsx
+++ src/components/stages/Stage3.tsx
@@ -2,15 +2,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
+import { useState, useEffect } from "react";
+import "../../styles/Stage3.scss";
 
 
-const Stage3: React.FC = () =>{
 
-    return(
-        <div className = "stage-3-container">
-            <h1>First Party vs Third Party Cookies</h1>
-            
+export default function Basket() {
+    const [basketX, setBasketX] = useState(200);
+    const basketWidth = 100;
+
+    //Basket Movement
+    useEffect(() => {
+        const handleKeyDown = (e:KeyboardEvent) =>{
+        if(e.key ==="ArrowLeft"){
+            setBasketX((x) => Math.max(0,x-30));
+        }
+        else if (e.key==="ArrowRight"){
+            setBasketX((x) => Math.min(window.innerWidth-basketWidth, x+30))
+        }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () =>window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+
+    return (
+        <div className = "gameArea">
+          <div className = "instructions">Press ← → to move</div>
+    
+          {/* basket (positioned absolutely using inline styles for clarity) */}
+          <div className = "basket"
+            style={{ left: basketX, width: basketWidth,}}
+          />
         </div>
-
-    )
-}
+      );
+    }
+    
