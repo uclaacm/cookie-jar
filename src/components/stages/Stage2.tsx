af78a406cb1ebd97ad98a2e72350e32359b2d4e6
Mirabel Vuong <158006280+miravuong@users.noreply.github.com>
2025-11-02 15:58:10 -0800

    38 frontend implement stage 2 design (#70)
    * Completed Stage 1 and began on Stage 2
    
    * Base of Stage 2 frontend design
    
    * Completed Stage 2
    
    * Minor changes to stage 2
    
    ---------
    
    Co-authored-by: Jarod Houston <jarodhouston26@gmail.com>

--- src/components/stages/Stage2.tsx
+++ src/components/stages/Stage2.tsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import "../../styles/Stage2.scss";
+import flour from "/assets/Flour.png";
+import nest from "/assets/Nest.png";
+import milk from "/assets/MilkCarton.png";
+import sugar from "/assets/SpoonofSugar.png";
+import knife from "/assets/Knife.png";
+import cookies from "/assets/Cookies.png";
+import bowl from "/assets/bowl.png";
+import bowlSubtract from "/assets/BowlSubtract.png";
+import eggs from "/assets/Eggs.png";
+import sugarMound from "/assets/Sugar.png";
 import { Link } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
-import "../../styles/Stage2.scss";
-
-import flour from "/assets/Flour.png";
+import {
+  Button,
+  Card,
+  ListItem,
+} from "../../../node_modules/@mui/material/index";
 
 const Stage2: React.FC = () => {
-  interface InteractiveCards {
+  interface Ingredient {
     id: number;
     name: string;
     img: string;
   }
+  const [hideCardsInBackground, setHideCardsInBackground] = useState(false);
+  const [selectedIngredient, setSelectedIngredient] = useState("");
+  const [addedIngredients, setAddedIngredients] = useState<Set<string>>(
+    new Set()
+  );
 
-  const cards: InteractiveCards[] = [
+  const ingredients: Ingredient[] = [
     {
       id: Date.now(),
       name: "FLOUR",
@@ -21,48 +40,121 @@ const Stage2: React.FC = () => {
     {
       id: Date.now() + 1,
       name: "EGGS",
-      img: flour,
+      img: nest,
     },
     {
       id: Date.now() + 2,
       name: "MILK",
-      img: flour,
+      img: milk,
     },
     {
       id: Date.now() + 3,
       name: "SUGAR",
-      img: flour,
+      img: sugar,
     },
     {
       id: Date.now() + 4,
       name: "BUTTER",
-      img: flour,
+      img: knife,
     },
     {
       id: Date.now() + 5,
       name: "TOPPINGS",
-      img: flour,
+      img: cookies,
     },
   ];
 
+  const ingredientDescriptions = {
+    FLOUR:
+      "Just like how flour is added to real cookies, login information (such as usernames and passwords) can be added and stored in web cookies!",
+    EGGS: "Your browsing history can be stored in web cookies as well. Let's add some eggs in to represent all the sites you have visited.",
+    MILK: "Personal data like name, address, and phone number can be stored in web cookies. Just like how milk isn't always added to real cookies, web cookies don't always contain this data!",
+    SUGAR: "Time to add something sweet! Technical details like your IP address, browser type, and operating system can be included in a web cookie!",
+    BUTTER:
+      "Web cookies can store items in a shopping cart on an online shopping website. This can make your shopping experience smooth as butter!",
+    TOPPINGS:
+      "Last but not least! Web cookies can store user preferences, like what language or font size you like to read and view website content with. Let's add some toppings of your choice!",
+  };
+
+  function greyOutCards() {
+    setHideCardsInBackground(true);
+  }
+
+  function selectIngredient(id: number) {
+    if (hideCardsInBackground) return; // don't allow user to select a card if one is already selected
+    const ingredientName = ingredients.find((item) => item.id === id).name;
+    if (addedIngredients.has(ingredientName)) {
+      return;
+    }
+    setSelectedIngredient(ingredientName);
+    greyOutCards();
+  }
+
+  function addIngredient() {
+    setAddedIngredients((prev) => new Set(prev).add(selectedIngredient));
+    setHideCardsInBackground(false);
+  }
 
   return (
     <div className="stage2-container">
-      <h1 style={{ marginBottom: "40px" }}>Stage 2</h1>
+      <h1 style={{ marginBottom: "30px" }}>Stage 2</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Let's see how web cookies are made! Just like mixing ingredients makes
+        cookie dough, websites mix bits of your info to make a web cookie.
       </p>
-
-      <Link to="/stage1" className="back-button">
-        <ArrowLeft />
-      </Link>
-
+      <div className="stage2-graphics-container">
+        <div className="ingredients-container">
+          {ingredients.map((card) => (
+            <div
+              key={card.id}
+              className={
+                hideCardsInBackground
+                  ? "ingredient-card-hidden"
+                  : addedIngredients.has(card.name)
+                    ? "ingredient-card ingredient-card-used"
+                    : "ingredient-card"
+              }
+              onClick={() => selectIngredient(card.id)}
+            >
+              {!hideCardsInBackground ? (
+                <>
+                  <img src={card.img} alt={`${card.name} icon`} />
+                  <p>{card.name}</p>
+                </>
+              ) : null}
+            </div>
+          ))}
+          {hideCardsInBackground && (
+            <div className="ingredient-card-selected">
+              <p>{ingredientDescriptions[selectedIngredient]}</p>
+              <button onClick={() => addIngredient()}>
+                <p>
+                  Add{" "}
+                  {selectedIngredient.charAt(0) +
+                    selectedIngredient.slice(1).toLowerCase()}
+                </p>
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="bowl-container">
+          <img
+            src={bowlSubtract}
+            alt="bowl"
+            style={{ marginBottom: "-35px", zIndex: "-10" }}
+          />
+          <img src={bowl} alt="bowl" />
+          {selectedIngredient === "EGGS" && hideCardsInBackground && (
+            <img src={eggs} alt="eggs" className="eggs-icon" />
+          )}
+          {selectedIngredient === "SUGAR" && hideCardsInBackground && (
+            <img src={sugarMound} alt="sugar" className="sugar-icon" />
+          )}
+        </div>
+      </div>
+      {addedIngredients.size === ingredients.length && (
+        <button className="next-stage-button">Next Stage</button>
+      )}
     </div>
   );
 };
