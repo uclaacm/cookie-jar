bc4efa9db32e492f1cea188104ac43800763f8f8
Andrew Li <207803156+andrewrli@users.noreply.github.com>
2025-11-04 19:54:28 -0800

    Improve styling

--- src/components/stages/Stage5.tsx
+++ src/components/stages/Stage5.tsx
@@ -1,8 +1,25 @@
 import React from "react"
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+import { ArrowRight } from "lucide-react";
+import "../../styles/Stage5.scss";
 
 const Stage5: React.FC = () => {
   return (
-    <h1>Hello world!</h1>
+    <div className="stage5-container">
+      <h1>Stage 5</h1>
+      <p>
+        Hello world!
+      </p>
+
+      <Link to="/stage4" className="back-button">
+        <ArrowLeft />
+      </Link>
+
+      <Link to="/stage6" className="next-button">
+        <ArrowRight />
+      </Link>
+    </div>
   );
 };
 
