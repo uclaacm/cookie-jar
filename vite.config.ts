f1cdeb8cb6308182ecbb3b2d7d661b91bf1e38c4
Mirabel Vuong <158006280+miravuong@users.noreply.github.com>
2025-12-03 01:28:19 -0800

    Pr 96 (#97)
    * Update Login.scss
    
    Improved Login page css
    
    * Falling Cookie Game adjustments
    
    Fixed collision detectors, so that it detects if cookie strikes both baskets that it's game over, adjusted cookie spawns so they spawn at least a basketwidth apart, adjusted cookie spawn so one spawns immediately, adjusted falling mechanics so they get faster with each cookie.
    
    * Changed gamespeeds
    
    * Port fix
    
    ---------
    
    Co-authored-by: GeeseAndGoosey <williamdiao03@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Proxy API requests to Express
+      '/api': 'http://localhost:3000', // Proxy API requests to Express
     }
   }
 })
