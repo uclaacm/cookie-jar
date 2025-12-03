85431c21f372ba5f7c62316c7a257173ba9dfbab
miravuong <158006280+miravuong@users.noreply.github.com>
2025-12-02 17:18:53 -0800

    Minor tweaks

--- src/components/stages/Stage5.tsx
+++ src/components/stages/Stage5.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import "../../styles/GameStage4.scss";
+import "../../styles/Stage5.scss";
 import { Link } from "react-router-dom";
 import {
   DragDropContext,
